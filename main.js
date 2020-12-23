4fc62e48ec54dd11ded027a5bf33124e94e81064
dwesty17 <j.dylan.westerhold@gmail.com>
2020-12-23 11:53:22 -0500

    Added sequelize

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('recipe_app', 'root', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+const main = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+};
+
+(async () => await main())();
