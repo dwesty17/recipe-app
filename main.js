24f6c26155058d3a997359ae20fcb9c694f5d9b5
dwesty17 <j.dylan.westerhold@gmail.com>
2020-12-23 11:59:10 -0500

    Use environment variables for DB credentials

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('recipe_app', 'root', 'password', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME || 'recipe_app',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASSWORD || '',
+    {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
+    }
+);
 
 const main = async () => {
     try {
